c87b3b102ce6f94544986df0469662467d6eb13e
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2023-06-03 23:55:37 +0200

    update cookie structure according to medium article

--- src/pages/api/[...nextauth].ts
+++ src/pages/api/[...nextauth].ts
@@ -2,8 +2,41 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { User } from "../../types";
 
+// This helper function will allows us to get the domain name regardless of its form
+// beta.example.com => example.com
+// example.com/* => example.com
+// localhost:3000 => localhost
+const getDomainWithoutSubdomain = (url: string) => {
+  const urlParts = new URL(url).hostname.split(".");
+
+  return urlParts
+    .slice(0)
+    .slice(-(urlParts.length === 4 ? 3 : 2))
+    .join(".");
+};
+
+const useSecureCookies = process.env.NEXTAUTH_URL?.startsWith("https://");
+const cookiePrefix = useSecureCookies ? "__Secure-" : "";
+const hostName = getDomainWithoutSubdomain(process.env.NEXTAUTH_URL!);
+
+// Define how we want the session token to be stored in our browser
+const cookies = {
+  sessionToken: {
+    name: `${cookiePrefix}next-auth.session-token`,
+    options: {
+      httpOnly: true,
+      sameSite: "lax",
+      path: "/",
+      secure: useSecureCookies,
+      domain: hostName == "localhost" ? hostName : "." + hostName, // add a . in front so that subdomains are included
+    },
+  },
+};
+
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
+  secret: process.env.NEXTAUTH_SECRET,
+  useSecureCookies,
   providers: [
     CredentialsProvider({
       credentials: {},
@@ -28,6 +61,7 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
   },
+  cookies,
   pages: {
     signIn: "/",
   },
